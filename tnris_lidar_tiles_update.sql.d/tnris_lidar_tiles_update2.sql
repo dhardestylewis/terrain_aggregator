e9e1e6bf794a10bd82d185558e7736fdcc007b62
Daniel Hardesty Lewis <danielhardestylewis@utexas.edu>
2022-03-17 17:39:52 -0500

    Update tnris_lidar_tiles_update2.sql

--- tnris_lidar_tiles_update.sql.d/tnris_lidar_tiles_update2.sql
+++ tnris_lidar_tiles_update.sql.d/tnris_lidar_tiles_update2.sql
@@ -5,3 +5,4 @@ UPDATE aaamissing SET centroid = ST_Centroid(envelope) ;
 UPDATE aaamissing SET centroid_albers = ST_Transform(centroid,6579) ;
 UPDATE aaamissing SET x = ST_X(centroid) ;
 UPDATE aaamissing SET y = ST_Y(centroid) ;
+INSERT INTO tnris_lidar_tiles (rast, filename, absolutepath, project, srid, srid_orig, pixeltype, envelope, envelope_albers, centroid, centroid_albers, x, y, width, height, pixelwidth, pixelheight) SELECT rast, filename, absolutepath, project, srid, srid_orig, pixeltype, envelope, envelope_albers, centroid, centroid_albers, x, y, width, height, pixelwidth, pixelheight FROM aaamissing ;
