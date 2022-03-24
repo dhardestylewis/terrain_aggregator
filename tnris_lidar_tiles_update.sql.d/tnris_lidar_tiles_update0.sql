afd55d57292c25fc0c5ea22759a6dd7d58107301
Daniel Hardesty Lewis <dhl@tacc.utexas.edu>
2022-03-23 19:34:33 -0500

    separated update scripts and provided context for variable names

---
+++ tnris_lidar_tiles_update.sql.d/tnris_lidar_tiles_update0.sql
@@ -0,0 +1,4 @@
+ALTER TABLE absolutepaths ADD COLUMN path_parts text[], ADD COLUMN project text, ADD COLUMN filename text ;
+UPDATE absolutepaths SET path_parts = REGEXP_SPLIT_TO_ARRAY(absolutepath,'/') ;
+UPDATE absolutepaths SET project = path_parts[6] ;
+UPDATE absolutepaths SET filename = path_parts[9] ;
