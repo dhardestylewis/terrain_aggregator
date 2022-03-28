e92349da95c43bb379e8f7299386e1e57f86b384
Daniel Hardesty Lewis <dhl@tacc.utexas.edu>
2022-03-28 16:38:31 -0500

    Retiling VRTs added

--- tnris_lidar_tiles_update.sql.d/tnris_lidar_tiles_update2.sql
+++ tnris_lidar_tiles_update.sql.d/tnris_lidar_tiles_update2.sql
@@ -7,4 +7,4 @@ UPDATE missing_dem_tiles SET x = ST_X(centroid) ;
 UPDATE missing_dem_tiles SET y = ST_Y(centroid) ;
 UPDATE missing_dem_tiles SET pixelwidth = ST_PixelWidth(rast) ;
 UPDATE missing_dem_tiles SET pixelheight = ST_PixelHeight(rast) ;
-INSERT INTO tnris_lidar_tiles (rast, filename, absolutepath, project, srid, srid_orig, pixeltype, envelope, envelope_albers, centroid, centroid_albers, x, y, width, height, pixelwidth, pixelheight) SELECT rast, filename, absolutepath, project, srid, srid_orig, pixeltype, envelope, envelope_albers, centroid, centroid_albers, x, y, width, height, pixelwidth, pixelheight FROM missing_dem_tiles ;
+/*INSERT INTO tnris_lidar_tiles (rast, filename, absolutepath, project, srid, srid_orig, pixeltype, envelope, envelope_albers, centroid, centroid_albers, x, y, width, height, pixelwidth, pixelheight) SELECT rast, filename, absolutepath, project, srid, srid_orig, pixeltype, envelope, envelope_albers, centroid, centroid_albers, x, y, width, height, pixelwidth, pixelheight FROM missing_dem_tiles ;*/
